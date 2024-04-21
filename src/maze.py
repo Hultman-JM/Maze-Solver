47c3a10a33407523e7217fbc44fe071ae8397b99
Hultman-JM <hultman.jm@yahoo.com>
2024-04-21 02:00:05 -0600

    CH 11-13  completed and the maze solver is completed

--- src/maze.py
+++ src/maze.py
@@ -22,6 +22,7 @@ class Maze:
         self._create_cells()
         self._break_entrance_and_exit()
         self._break_walls_r(0,0)
+        self._reset_cells_visited()
     
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -110,3 +111,83 @@ class Maze:
                 self._draw_cell(i, j)
                 self._draw_cell(r_dir[0],r_dir[1])
                 self._break_walls_r(r_dir[0],r_dir[1])
+
+    def _reset_cells_visited(self):
+        for col in self._cells:
+            for cell in col:
+                cell.visited = False
+
+    def is_wall_between(self, cell_coord, i, j):
+        cur_cell = self._cells[i][j]
+        #left from cur
+        if cell_coord[0] < i:
+            return cur_cell.has_left_wall
+        #up from cur
+        if cell_coord[1] < j:
+            return cur_cell.has_top_wall
+        #right from cur
+        if cell_coord[0] > i:
+            return cur_cell.has_right_wall
+        #down from cur
+        if cell_coord[1] > j:
+            return cur_cell.has_bottom_wall
+
+    def solve(self):
+        if self._solve_r(0,0) == True:
+            print("You've Reached the Finish!")
+            return True
+        else:
+            return False
+    
+    def _solve_r(self,i,j):
+        self._animate()
+        cur_cell = self._cells[i][j]
+        cur_cell.visited = True
+        end_cell = self._cells[self._num_cols-1][self._num_rows-1]
+
+        if cur_cell == end_cell:
+            return True
+        else:
+            cells_to_move_to = []
+            directions = []
+            coords= []
+            #load possible directions and their coords in the grid
+            if i-1 >= 0: 
+                directions.append("left")
+                cells_to_move_to.append(self._cells[i-1][j])
+                coords.append((i-1, j))
+            if  j-1 >= 0:
+                directions.append("up")
+                cells_to_move_to.append(self._cells[i][j-1])
+                coords.append((i, j-1))
+            if i+1 <= self._num_cols-1:
+                directions.append("right")
+                cells_to_move_to.append(self._cells[i+1][j])
+                coords.append((i+1, j))
+            if j+1 <= self._num_rows-1:
+                directions.append("down")
+                cells_to_move_to.append(self._cells[i][j+1])
+                coords.append((i, j+1))
+
+            cell_coord = ()
+            if len(directions) != 0:
+                #for each direction
+                for to_cell in cells_to_move_to:
+                    cell_coord = coords.pop(0)
+                    direction = directions.pop(0)
+                    #no wall and the cell hasnt been visited
+                    if self.is_wall_between(cell_coord, i, j) == False and to_cell.visited == False:
+                        #draw a move
+                        cur_cell.draw_move(to_cell)
+                        #move with to the cell
+                        #is the desitination a dead end?
+                        if self._solve_r(cell_coord[0], cell_coord[1]) == False:
+                            dead_end_cell = self._cells[cell_coord[0]][cell_coord[1]]
+                            dead_end_cell.draw_move(cur_cell, True) 
+                        else:
+                            return True
+                #no directions were successful, mark cur_cell as not visited and return False to show that cur_cell is a dead end?
+                cur_cell.visited = False
+                return False
+                
+            
